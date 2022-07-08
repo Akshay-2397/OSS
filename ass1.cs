c6bec7c2a5b538a8fcdeec6922b7277248834d96
Akshay-2397 <108930274+Akshay-2397@users.noreply.github.com>
2022-07-08 15:37:26 +0530

    assignment uploaded

---
+++ ass1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int count = 56;
+            count = count + 1;
+            Console.WriteLine(count);
+            Console.WriteLine("Hello .NET , Welcome to .NET World...");
+
+
+        }
+    }
+}
